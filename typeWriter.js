444476b0f7e9a0fced1f8c08c51fe189f46bedd3
hanasay <jerrytommy2009@gmail.con>
2023-04-25 20:20:21 +0800

    Add in main typeWtier effect, with js css.

---
+++ typeWriter.js
@@ -0,0 +1,19 @@
+var messageArray = ["asr_dev=[kaldi, espnet]", "game_dev=[Unity, Unreal]", "web_dev=[node.js, html]", "lang=[cht, eng, C++, C#, python, bash]"];
+var textPosition = 0;
+var speed = 100;
+var index = 0;
+
+typewriter = () => {
+    document.querySelector("#para"+(index+1)).
+    innerHTML = messageArray[index].substring(0,textPosition);
+        
+    if(textPosition++ != messageArray[index].length)
+        setTimeout(typewriter, speed);
+    else {
+        textPosition = 0;
+        index++;
+        setTimeout(typewriter, speed);
+    }
+}
+
+window.addEventListener("load", typewriter)
