6ed4dff786f54436d1e029f97138743a8b773d89
hanasay <hanasay16@gmail.com>
2024-04-20 20:19:35 +0800

    Update Klee gif

--- JavaScript/typeWriter.js
+++ JavaScript/typeWriter.js
@@ -1,7 +1,7 @@
 var messageArray = ["My name is Yi-Jun Zheng. I'm a new born software engineer.",
+                    "Being a digital artist's mentor, guiding Generative AI to paint worlds of imagination with strokes of data.",
                     "Graduated From: National Pintung University, Majoring in Computer Science ",
-                    "Now I am an AI Engineer. Expert at Large Language Model & Speech AI",
-                    "Being a digital artist's mentor, guiding Generative AI to paint worlds of imagination with strokes of data."];
+                    "Now I am an AI Engineer. Expert at Large Language Model & Speech AI"];
 var textPosition = 0;
 var speed = 30;
 var index = 0;
